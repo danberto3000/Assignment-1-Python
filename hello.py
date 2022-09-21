ebd18531568af4b58f7bd510ed48b0c92c1afadb
uncledan <uncledan@gmail.com>
2022-09-21 10:08:59 +0000

    Initial commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+#This program says hello and asks for my name
+
+print('Hello, World')
+
+print('What is your name?')
+
+myName = input()
+
+print('It is good to meet you, ' + myName)
+
+a = len(myName)
+
+print('The length of your name is ' + str(int(a)) )
+
+
+print('What is your age?')
+
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 10) + ' in 10 years.')
